a905f51c10402072de850725a247e91b12085da7
Prahalad Chari <pchar4@gmail.com>
2024-11-30 21:29:31 -0600

    zhongqi code update for rtl blackbox testing

---
+++ zhongqi_files/hls_component/accelerator/hls/csim/build/csim.mk
@@ -0,0 +1,127 @@
+# ==============================================================
+# Vitis HLS - High-Level Synthesis from C, C++ and OpenCL v2024.1 (64-bit)
+# Tool Version Limit: 2024.05
+# Copyright 1986-2022 Xilinx, Inc. All Rights Reserved.
+# Copyright 2022-2024 Advanced Micro Devices, Inc. All Rights Reserved.
+# 
+# ==============================================================
+CSIM_DESIGN = 1
+
+__SIM_FPO__ = 1
+
+__SIM_MATHHLS__ = 1
+
+__SIM_FFT__ = 1
+
+__SIM_FIR__ = 1
+
+__SIM_DDS__ = 1
+
+__USE_CLANG__ = 1
+
+__USE_VCXX_CLANG__ = 1
+
+ObjDir = obj
+
+HLS_SOURCES = ../../../../../main.cpp ../../../../../weight_pe.cpp ../../../../../accelerator.cpp ../../../../../error_pe.cpp ../../../../../bias_pe.cpp ../../../../../array.cpp ../../../../../act_pe.cpp
+
+override TARGET := csim.exe
+
+AUTOPILOT_ROOT := C:/Xilinx/Vitis_HLS/2024.1
+AUTOPILOT_MACH := win64
+ifdef AP_GCC_M32
+  AUTOPILOT_MACH := Linux_x86
+  IFLAG += -m32
+endif
+ifndef AP_GCC_PATH
+  AP_GCC_PATH := C:/Xilinx/Vitis_HLS/2024.1/tps/win64/msys64/mingw64/bin
+endif
+AUTOPILOT_TOOL := ${AUTOPILOT_ROOT}/${AUTOPILOT_MACH}/tools
+AP_CLANG_PATH := ${XILINX_VCXX}/libexec
+AUTOPILOT_TECH := ${AUTOPILOT_ROOT}/common/technology
+
+
+IFLAG += -I "${AUTOPILOT_ROOT}/include"
+IFLAG += -I "${AUTOPILOT_ROOT}/include/ap_sysc"
+IFLAG += -I "${AUTOPILOT_TECH}/generic/SystemC"
+IFLAG += -I "${AUTOPILOT_TECH}/generic/SystemC/AESL_FP_comp"
+IFLAG += -I "${AUTOPILOT_TECH}/generic/SystemC/AESL_comp"
+IFLAG += -I "${AUTOPILOT_TOOL}/auto_cc/include"
+IFLAG += -D__HLS_COSIM__
+
+IFLAG += -D__HLS_CSIM__
+
+IFLAG += -D__VITIS_HLS__
+
+IFLAG += -D__SIM_FPO__
+
+IFLAG += -D__SIM_FFT__
+
+IFLAG += -D__SIM_FIR__
+
+IFLAG += -D__SIM_DDS__
+
+IFLAG += -D__DSP48E2__
+LFLAG += -Wl,--stack,0x40000000
+IFLAG += -g
+IFLAG += -DNT
+LFLAG += -Wl,--enable-auto-import 
+DFLAG += -D__xilinx_ip_top= -DAESL_TB
+CCFLAG += -Werror=return-type
+CCFLAG += -Wno-abi
+CCFLAG += -fdebug-default-version=4
+CCFLAG += --sysroot=C:/Xilinx/Vitis_HLS/2024.1/tps/mingw/10.0.0/win64.o/nt
+CCFLAG += -Werror=uninitialized
+CCFLAG += -Wno-c++11-narrowing
+CCFLAG += -Wno-error=sometimes-uninitialized
+LFLAG += --sysroot=C:/Xilinx/Vitis_HLS/2024.1/tps/mingw/10.0.0/win64.o/nt
+
+
+
+include ./Makefile.rules
+
+all: $(TARGET)
+
+
+
+$(ObjDir)/main.o: ../../../../../main.cpp $(ObjDir)/.dir
+	$(Echo) "   Compiling ../../../../../main.cpp in $(BuildMode) mode" $(AVE_DIR_DLOG)
+	$(Verb)  $(CXX) -std=gnu++14 ${CCFLAG} -c -MMD -Wno-unknown-pragmas -Wno-unknown-pragmas  $(IFLAG) $(DFLAG) $< -o $@ ; \
+
+-include $(ObjDir)/main.d
+
+$(ObjDir)/weight_pe.o: ../../../../../weight_pe.cpp $(ObjDir)/.dir
+	$(Echo) "   Compiling ../../../../../weight_pe.cpp in $(BuildMode) mode" $(AVE_DIR_DLOG)
+	$(Verb)  $(CXX) -std=gnu++14 ${CCFLAG} -c -MMD  $(IFLAG) $(DFLAG) $< -o $@ ; \
+
+-include $(ObjDir)/weight_pe.d
+
+$(ObjDir)/accelerator.o: ../../../../../accelerator.cpp $(ObjDir)/.dir
+	$(Echo) "   Compiling ../../../../../accelerator.cpp in $(BuildMode) mode" $(AVE_DIR_DLOG)
+	$(Verb)  $(CXX) -std=gnu++14 ${CCFLAG} -c -MMD  $(IFLAG) $(DFLAG) $< -o $@ ; \
+
+-include $(ObjDir)/accelerator.d
+
+$(ObjDir)/error_pe.o: ../../../../../error_pe.cpp $(ObjDir)/.dir
+	$(Echo) "   Compiling ../../../../../error_pe.cpp in $(BuildMode) mode" $(AVE_DIR_DLOG)
+	$(Verb)  $(CXX) -std=gnu++14 ${CCFLAG} -c -MMD  $(IFLAG) $(DFLAG) $< -o $@ ; \
+
+-include $(ObjDir)/error_pe.d
+
+$(ObjDir)/bias_pe.o: ../../../../../bias_pe.cpp $(ObjDir)/.dir
+	$(Echo) "   Compiling ../../../../../bias_pe.cpp in $(BuildMode) mode" $(AVE_DIR_DLOG)
+	$(Verb)  $(CXX) -std=gnu++14 ${CCFLAG} -c -MMD  $(IFLAG) $(DFLAG) $< -o $@ ; \
+
+-include $(ObjDir)/bias_pe.d
+
+$(ObjDir)/array.o: ../../../../../array.cpp $(ObjDir)/.dir
+	$(Echo) "   Compiling ../../../../../array.cpp in $(BuildMode) mode" $(AVE_DIR_DLOG)
+	$(Verb)  $(CXX) -std=gnu++14 ${CCFLAG} -c -MMD  $(IFLAG) $(DFLAG) $< -o $@ ; \
+
+-include $(ObjDir)/array.d
+
+$(ObjDir)/act_pe.o: ../../../../../act_pe.cpp $(ObjDir)/.dir
+	$(Echo) "   Compiling ../../../../../act_pe.cpp in $(BuildMode) mode" $(AVE_DIR_DLOG)
+	$(Verb)  $(CXX) -std=gnu++14 ${CCFLAG} -c -MMD  $(IFLAG) $(DFLAG) $< -o $@ ; \
+
+-include $(ObjDir)/act_pe.d

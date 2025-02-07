738ca9b749d94556df6ec6511febc221009fe109
N-Mendiola <nm58@rice.edu>
2025-02-06 19:46:16 -0600

    WIP c sim

--- split_model_logic/c_testing/test_c_main.cpp
+++ split_model_logic/c_testing/test_c_main.cpp
@@ -1,20 +1,34 @@
+/*
+This is the test bench file for the GIM@Rice accelerator 
+C sim of distributed learning
+
+Natalia Mendiola, 2/6/2025
+test_c_main.cpp: Acts as the main client that connects to the server and 
+runs the neural network calculations, handling data transfer, and updates.
+
+*/
 #include <iostream>
 #include <string>
 #include <memory>
 #include <cstring>
 #include <winsock2.h>
 #include <ws2tcpip.h>
+#include "gim_model.h"
 
 constexpr int PORT = 12345;
 constexpr int BUFFER_SIZE = 1024;
-constexpr int NUM_ITERATIONS = 3;
-constexpr int ARRAY_SIZE = 2;
 
 using namespace std;
 
-typedef int16_t fixed_16;
 
 int main() {
+    Inference output_array;
+
+    //inputs to function from cpp_accelerator_generic/accelerator.cpp
+    //------values from prahalad_xor_bringup/gim_cpp_final/main.cpp
+    fixed_16 bias_1[ARRAY_SIZE] = {0.50524609, 0.0652865};
+    fixed_16 w1[ARRAY_SIZE][ARRAY_SIZE] = {{0.13457995, 0.51357812}, {0.18443987, 0.78533515}};
+
     //Communication
     //------------------------
     // Initialize Winsock
@@ -70,48 +84,153 @@ int main() {
         return 1;
     }
 
-    //Initialize variables to send
-    fixed_16 bias_change[ARRAY_SIZE] = {1,0};
-    fixed_16 weight_change[ARRAY_SIZE][ARRAY_SIZE] = {{1,0},{1,1}};
-    fixed_16 output_k[ARRAY_SIZE][ARRAY_SIZE] = {{0,1},{0,0}};
+    //based on values from prahalad_xor_bringup/gim_cpp_final/accelerator.cpp
+    /*
+    */
+    //Initialize variables needed
+    fixed_16 data[2][4] = {{0, 0, 1, 1}, {0, 1, 0, 1}};
+    fixed_16 output_0[ARRAY_SIZE] = {};
+    fixed_16 output_1[ARRAY_SIZE] = {};
+    fixed_16 delta_1[ARRAY_SIZE] = {};
 
-    for (int n=0; n<NUM_ITERATIONS; n++){
-        cout << "Iteration: " << n << endl;
-        //Send data from server to client
-        send(new_socket, reinterpret_cast<char*>(bias_change), sizeof(bias_change), 0);
-        send(new_socket, reinterpret_cast<char*>(weight_change),sizeof(weight_change),0);
-        send(new_socket, reinterpret_cast<char*>(output_k),sizeof(output_k), 0);
-        cout << "Send bias, weight, and outpu to client." <<endl;
+    Array array_out2;
+    Array array_back2;
 
-        // Receive acknowledgment from client
-        ssize_t valread = recv(new_socket, buffer, BUFFER_SIZE, 0);
-        if (valread > 0) {
-            buffer[valread] = '\0';
-            cout << "Received acknowledgment: " << buffer << endl;
-        } else {
-            cerr << "Error reading from socket" << endl;
+    // make local versions of the weights/biases
+    fixed_16 w1_local[ARRAY_SIZE][ARRAY_SIZE] = {};
+    fixed_16 bias_1_local[ARRAY_SIZE] = {};
+    for (int n = 0; n < ARRAY_SIZE; n++) {
+        bias_1_local[n] = bias_1[n];
+        for (int m = 0; m < ARRAY_SIZE; m++) {
+            w1_local[n][m] = w1[n][m];
         }
-        
-        // Increment each element in bias_change, weight_change, and output_k
-        for (int i = 0; i < ARRAY_SIZE; i++) {
-            bias_change[i] += 1;
-            for (int j = 0; j < ARRAY_SIZE; j++) {
-                weight_change[i][j] += 1;
-                output_k[i][j] += 1;
+    }
+
+    // number of iterations defined in the header file
+    // store actual and predicted difference in vector, set other params
+    char model = 'r';
+    fixed_16 alpha = 0.1; // for leaky relu
+    fixed_16 lr = 0.1; // learning rate
+
+    for (int i=0; i<NUM_ITERATIONS;i++){
+        for(int j=0; j<DATA_SIZE; j++){ //go through each data point
+            //Set up initial input
+            for (int p = 0; p < ARRAY_SIZE; p++) {
+                // setup the initial data input
+                output_0[p] = data[p][j];
+                // initialize the error backpropagation
+                delta_1[p] = 0;
+            }
+            //FORWARD PASS ----- Layer 1
+            Array array_out1 = model_array(w1_local, bias_1_local, output_0, delta_1, lr, model, alpha, TRAINING);
+            for (int o = 0; o < ARRAY_SIZE; o++){
+                output_1[o] = array_out1.output_k[o];
+            }
+
+            // send output_1 to sub board
+            int bytesSent;
+            bytesSent = send(new_socket, reinterpret_cast<char*>(output_1), sizeof(output_1), 0);
+            if (bytesSent == SOCKET_ERROR) {
+                cerr << "Error sending output_1: " << WSAGetLastError() << endl;
+            }
+            // Receive output_2 from sub board
+            ssize_t bytesReceived;
+            bytesReceived = recv(new_socket, reinterpret_cast<char*>(&array_out2), sizeof(array_out2), 0);
+            if (bytesReceived == SOCKET_ERROR) {
+                cerr << "Error receiving forward propogation array_out2." << endl;
+            } else {
+                cout << "Received array_out2 from client. " << endl;
+            }
+
+            
+            bytesReceived = recv(new_socket, reinterpret_cast<char*>(&array_back2), sizeof(array_back2), 0);
+            if (bytesReceived == SOCKET_ERROR) {
+                cerr << "Error receiving backpropagation array_back2." << endl;
+            } else {
+                cout << "Received array_back2 from client." << endl;
+            }
+
+            for (int e = 0; e < ARRAY_SIZE; e++) {
+                delta_1[e] = array_back2.delta_kmin1[e];
+            }
+
+
+            //BACKPROPOGATION ---
+            Array array_back1 = model_array(w1_local, bias_1_local, output_0, delta_1, lr, model, alpha, TRAINING);
+ 
+            //update local w1, bias_1
+            for (int n = 0; n < ARRAY_SIZE; n++) {
+                bias_1_local[n] = array_back1.bias_change[n];
+                for (int m = 0; m < ARRAY_SIZE; m++) {
+                    w1_local[n][m] = array_back1.weight_changes[n][m];
+                }
+            }
+            if ((TRAINING==0)&&(j == (DATA_SIZE-1))){
+                break;// only run this for all data points once if infering
             }
         }
-
+        if (TRAINING == 0){
+            break; // only run this once if we are infering
         }
-
-    cout<<"Ran through all epoch! Sending termination signal"<<endl;
+    }
     
-    string end_signal = "END";
-    send(new_socket, end_signal.c_str(),end_signal.size(),0);
-    cout << "Training complete. Sent termination signal to client." << endl;
+    fixed_16 bias_2_local[ARRAY_SIZE] = {};
+    fixed_16 w2_local[ARRAY_SIZE][ARRAY_SIZE] = {};
+    // update the weights and biases
+    for (int n = 0; n < ARRAY_SIZE; n++) {
+        bias_2_local[n] = array_back2.bias_change[n];
+        for (int m = 0; m < ARRAY_SIZE; m++) {
+            w2_local[n][m] = array_back2.weight_changes[n][m];
+        }
+    }
+
+    // produce the final weights to be used in inference
+    for (int n = 0; n<ARRAY_SIZE; n++) {
+        output_array.new_b1[n] = bias_1_local[n];
+        output_array.new_b2[n] = bias_2_local[n];
+        for (int m = 0;m<ARRAY_SIZE; m++) {
+            output_array.new_w1[n][m] = w1_local[n][m];
+            output_array.new_w2[n][m] = w2_local[n][m];
+        }
+    }
 
     // Close the sockets
     closesocket(new_socket);
     closesocket(server_fd);
     WSACleanup();
+    //----------check resultls if they make sense
+    // Print new_w1
+    cout<<"-----" << endl;
+    cout << "new_w1:" << endl;
+    for (int i = 0; i < ARRAY_SIZE; i++) {
+        for (int j = 0; j < ARRAY_SIZE; j++) {
+            cout << output_array.new_w1[i][j] << " ";
+        }
+        cout << endl;
+    }
+
+    // Print new_w2
+    cout << "new_w2:" << endl;
+    for (int i = 0; i < ARRAY_SIZE; i++) {
+        for (int j = 0; j < ARRAY_SIZE; j++) {
+            cout << output_array.new_w2[i][j] << " ";
+        }
+        cout << endl;
+    }
+
+    // Print new_b1
+    cout << "new_b1: ";
+    for (int i = 0; i < ARRAY_SIZE; i++) {
+        cout << output_array.new_b1[i] << " ";
+    }
+    cout << endl;
+
+    // Print new_b2
+    cout << "new_b2: ";
+    for (int i = 0; i < ARRAY_SIZE; i++) {
+        cout << output_array.new_b2[i] << " ";
+    }
+    cout << endl;
+
     return 0;
 }
--- split_model_logic/c_testing/test_c_sub.cpp
+++ split_model_logic/c_testing/test_c_sub.cpp
@@ -1,19 +1,35 @@
+/* This is the test bench file for the GIM@Rice accelerator 
+C sim of distributed learning
+
+Natalia Mendiola, 2/6/2025
+
+test_c_sub.cpp: Handles the core neural network computation logic 
+(forward propagation, backpropagation, error calculation) and communicates 
+results back to the server.
+*/
 #include <iostream>
 #include <string>
 #include <memory>
 #include <cstring>
 #include <winsock2.h>
 #include <ws2tcpip.h>
+#include "gim_model.h"
 
 constexpr int PORT = 12345;
 constexpr int BUFFER_SIZE = 1024;
-constexpr int ARRAY_SIZE = 2;
+// constexpr int ARRAY_SIZE = 2;
 constexpr int TIMEOUT = 5000; //5 sec timeout
+
 using namespace std;
 
-typedef int16_t fixed_16;
+// typedef fixed_16 fixed_16;
 
 int main() {
+    //inputs to function from cpp_accelerator_generic/accelerator.cpp
+    //------values from prahalad_xor_bringup/gim_cpp_final/main.cpp
+    fixed_16 bias_2[ARRAY_SIZE] = {0.42812233, 0};
+    fixed_16 w2[ARRAY_SIZE][ARRAY_SIZE] = {{0.85397529, 0.49423684}, {0, 0}};
+
     //Communication
     //------------------------
     // Initialize Winsock
@@ -54,21 +70,42 @@ int main() {
 
     //------------------------
     cout << "Connected to server. Waiting for data..." << endl;
-    // Continuous loop for receiving data
-    while(true){     
-        // Initialize variables to receive
-        fixed_16 bias_change[ARRAY_SIZE];
-        fixed_16 weight_change[ARRAY_SIZE][ARRAY_SIZE];
-        fixed_16 output_k[ARRAY_SIZE][ARRAY_SIZE];
+
+    //based  on from cpp_accelerator_generic/accelerator.cpp
+    fixed_16 y[4] = {0, 1, 1, 0};
+
+    // initializing internal arrays with zeros
+    fixed_16 output_2[ARRAY_SIZE] = {0, 0};
+    fixed_16 delta_2[ARRAY_SIZE] = {};
+
+    // make local versions of the weights/biases
+    fixed_16 w2_local[ARRAY_SIZE][ARRAY_SIZE] = {{0, 0}, {0, 0}};
+    fixed_16 bias_2_local[ARRAY_SIZE] = {0, 0};
+    for (int n = 0; n<ARRAY_SIZE; n++) {
+        bias_2_local[n] = bias_2[n];
+        for (int m = 0;m<ARRAY_SIZE; m++) {
+            w2_local[n][m] = w2[n][m];
+        }
+    }
+    // number of iterations defined in the header file
+    // store actual and predicted difference in vector, set other params
+    char model = 'r';
+    fixed_16 alpha = 0.1; // for leaky relu
+    fixed_16 lr = 0.1; // learning rate
+
+    // Continuous loop for receiving data 
+
+    while(true){ //this runs for each data point
 
         // Check for END signal first
         int bytesReceived = recv(sock, buffer, BUFFER_SIZE, 0);
         if (bytesReceived > 0) {
             buffer[bytesReceived] = '\0';  // Null terminate the buffer
             if (strcmp(buffer, "END") == 0) {
-                cout << "Received termination signal. Stopping client." << endl;
+                cout << "Received termination signal.  Stopping client." << endl;// Making inferences and calculating final error." << endl;
+                // cout << "sending back return array and stopping client." << endl;
                 break;
-            }
+            }            
         } else if (bytesReceived == SOCKET_ERROR) {
             if (WSAGetLastError() == WSAETIMEDOUT) {
                 cerr << "Receive operation timed out." << endl;
@@ -77,9 +114,9 @@ int main() {
             }
             break;
         }
-
-        // Receive bias_change
-        bytesReceived = recv(sock, reinterpret_cast<char*>(bias_change), sizeof(bias_change), 0);
+        // Receive output_1 from main board
+        fixed_16 output_1[ARRAY_SIZE];
+        bytesReceived = recv(sock, reinterpret_cast<char*>(output_1), sizeof(output_1), 0);
         if (bytesReceived == SOCKET_ERROR) {
             if (WSAGetLastError() == WSAETIMEDOUT) {
                 cerr << "Receive operation timed out." << endl;
@@ -88,56 +125,43 @@ int main() {
             }
             break;
         }
-
-        // Receive weight_change
-        bytesReceived = recv(sock, reinterpret_cast<char*>(weight_change), sizeof(weight_change), 0);
-        if (bytesReceived == SOCKET_ERROR) {
-            if (WSAGetLastError() == WSAETIMEDOUT) {
-                cerr << "Receive operation timed out." << endl;
-            } else {
-                cerr << "Error receiving data." << endl;
-            }
-            break;
-        }
-
-        // Receive output_k
-        bytesReceived = recv(sock, reinterpret_cast<char*>(output_k), sizeof(output_k), 0);
-        if (bytesReceived == SOCKET_ERROR) {
-            if (WSAGetLastError() == WSAETIMEDOUT) {
-                cerr << "Receive operation timed out." << endl;
-            } else {
-                cerr << "Error receiving data." << endl;
-            }
-            break;
-        }
-
-        cout << "Received data and labels from server." << endl;
         // Echo received data
-        cout << "Bias: "<<endl;
-        for (int row = 0; row < ARRAY_SIZE; row++){
-            cout << bias_change[row] << " ";
+        cout << "Output 1 received: " << endl;
+        for (int row = 0; row < ARRAY_SIZE; row++) {
+            cout << output_1[row] << " ";  // Print the element at position 'row' in the 1D array
         }
         cout << endl;
-        
-        cout << "Weights: "<<endl;
-        for (int row = 0; row < ARRAY_SIZE; row++){
-            for(int col = 0; col < ARRAY_SIZE; col++){
-                cout << weight_change[row][col] << " ";
-            }
-            cout << endl;
-        }
 
-        cout << "Output k received: "<<endl;
-        for (int row = 0; row < ARRAY_SIZE; row++){
-            for(int col = 0; col < ARRAY_SIZE; col++){
-                cout << output_k[row][col] << " ";
-            }
-            cout << endl;
-        }
+        //initialize the error backpropogation count
+        delta_2[0] = 0; 
+        delta_2[1] = 0;
 
-        // Update data before sending back?
-        string ack = "Data received!";
-        send(sock, ack.c_str(), ack.size(), 0);
+        //Forward propogation of layer 2
+        Array array_out2 = model_array(w2_local, bias_2_local, output_1, delta_2, lr, model, alpha, TRAINING);
+
+        // run the backpropagation and update the array
+        // start with layer 2
+        Array array_back2 = model_array(w2_local, bias_2_local, output_1, delta_2, lr, model, alpha, TRAINING);
+        // update the weights and biases
+        for (int n = 0; n < ARRAY_SIZE; n++) {
+            bias_2_local[n] = array_back2.bias_change[n];
+            for (int m = 0; m < ARRAY_SIZE; m++) {
+                w2_local[n][m] = array_back2.weight_changes[n][m];
+            }
+        }
+        cout << "Size of array_out2: " << sizeof(array_out2) << endl;
+        cout << "Size of array_back2: " << sizeof(array_back2) << endl;
+
+        int bytesSent;
+        //send w2_local and bias_2_local to main board
+        bytesSent= send(sock, reinterpret_cast<char*>(&array_out2), sizeof(array_out2), 0);
+        if (bytesSent == SOCKET_ERROR) {
+            cerr << "Error sending array_out2: " << WSAGetLastError() << endl;
+        }
+        bytesSent= send(sock, reinterpret_cast<char*>(&array_back2), sizeof(array_back2), 0);
+        if (bytesSent == SOCKET_ERROR) {
+            cerr << "Error sending array_back2: " << WSAGetLastError() << endl;
+        }
     }
 
     cout << "Server disconnected. Closing client." << endl;

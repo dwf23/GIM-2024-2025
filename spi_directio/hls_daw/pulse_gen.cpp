8af4b87f08a7315260ef2d87c8a97cbedebe3926
Dawson Franklin <90641138+dwf23@users.noreply.github.com>
2025-02-21 10:29:02 -0600

    Update pulse_gen.cpp
    quick add to pulse gen pragmas

--- spi_directio/hls_daw/pulse_gen.cpp
+++ spi_directio/hls_daw/pulse_gen.cpp
@@ -3,6 +3,8 @@
 
 bool pulse_gen(bool start){
     #pragma HLS INTERFACE mode=s_axilite port=start
+    #pragma HLS INTERFACE mode=s_axilite port=return
+    #pragma HLS INTERFACE ap_ctrl_non port=return
 
 
     return true;

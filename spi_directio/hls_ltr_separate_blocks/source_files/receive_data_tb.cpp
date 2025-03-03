f387da007cce45f205abcbe43155c80fad0c56a0
Lindsey Russ <ltr1@rice.edu>
2025-03-03 16:46:14 -0600

    slight modifications to receive_data

---
+++ spi_directio/hls_ltr_separate_blocks/source_files/receive_data_tb.cpp
@@ -0,0 +1,40 @@
+#include <cmath>
+#include <iostream>
+#include "hls_stream.h"
+#include "GIM_comm.h"
+#include "ap_int.h"
+
+
+
+int main()
+{
+    std::cout << "Starting Testbench" << std::endl;;
+    packet_line data_in;
+    comm_line alpha_tx;
+    pkt tmp;
+    volatile bool flag = true;
+    int interval = 100;
+    pkt data_received;
+
+
+#ifdef HW_COSIM
+   // Run the Vitis HLS block and pass pointer to r_hw to allow writing of variable
+    std::cout << "Beginning HLS Func" << std::endl;;
+    std::thread receive_data_thread(receive_data, std::ref(data_in), std::ref(alpha_tx), std::ref(flag));
+    for (int i = 0; i < 64; i++){
+        for (int j = 0; j < interval; j++);  // Delay for the interval
+        alpha_tx.write(i % 2);
+    }
+    data_in.read(data_received);
+    std::cout << "Received values " <<  data_received.data[0] << " and " << data_received.data[1] << std::endl;
+    flag = false;
+    receive_data_thread.join();
+
+    flag = false;
+    tmp = receive_data(data_in, alpha_tx, flag);
+    
+
+#endif
+
+   return 0;
+}

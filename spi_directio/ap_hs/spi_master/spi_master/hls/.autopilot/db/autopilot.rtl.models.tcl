e89b77d1440d5b24058a56ca28deb98e4635dda9
dwf23 <dwf2@rice.edu>
2025-04-16 22:01:01 -0500

    deleting more files

--- spi_directio/ap_hs/spi_master/spi_master/hls/.autopilot/db/autopilot.rtl.models.tcl
+++
@@ -1,3 +0,0 @@
-set SynModuleInfo {
-  {SRCNAME spi_master MODELNAME spi_master RTLNAME spi_master IS_TOP 1}
-}
--- spi_directio/ap_hs/spi_master/spi_master/hls/.autopilot/db/kernel_module_hierarchy.tcl
+++
@@ -1,5 +0,0 @@
-set ModuleHierarchy {[{
-"Name" : "spi_master","ID" : "0","Type" : "sequential",
-"SubLoops" : [
-	{"Name" : "VITIS_LOOP_18_1","ID" : "1","Type" : "pipeline"},]
-}]}
